53a45b07357f17f60887cb9b9af2cab43f6cf61e
raphew <stoleckipawel98@gmail.com>
2023-12-22 21:02:53 +0100

    name changes

--- Raytraycer/premake5.lua
+++ Raytraycer/premake5.lua
@@ -1,4 +1,4 @@
-project "WalnutApp"
+project "Raytraycer"
    kind "ConsoleApp"
    language "C++"
    cppdialect "C++17"
--- premake5.lua
+++ premake5.lua
@@ -1,10 +1,10 @@
 -- premake5.lua
-workspace "WalnutApp"
+workspace "Raytraycer"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "WalnutApp"
+   startproject "Raytraycer"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
 include "WalnutExternal.lua"
-include "WalnutApp"
+include "Raytraycer"
