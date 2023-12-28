8dda140f7b003417a9ec6a1219abe968fbe51e01
Paweł Stolecki <pawel.stolecki@pl.cdprojekt.com>
2023-12-28 23:41:24 +0100

    analytical solution to a sphere

--- Raytraycer/src/Scene/Geometry/Sphere.cpp
+++ Raytraycer/src/Scene/Geometry/Sphere.cpp
@@ -9,7 +9,7 @@ void Sphere::BuildUI(uint32_t id)
 	ImGui::PopID();
 }
 
-Trace Sphere::TraceRay(const Ray& ray) const
+Trace Sphere::GeometricSolution(const Ray& ray) const
 {
 	//src: https://www.scratchapixel.com/lessons/3d-basic-rendering/minimal-ray-tracer-rendering-simple-shapes/ray-sphere-intersection.html
 	glm::vec3 L = Position - ray.Origin;
@@ -29,10 +29,71 @@ Trace Sphere::TraceRay(const Ray& ray) const
 	return Trace().Hit(closestT, Guid);
 }
 
+Trace Sphere::AnalyticSolution(const Ray& ray) const
+{	
+	//O - Ray Origin
+	//D - Ray Direciton
+	//t - ray distance
+	//R - sphere Radius
+	//C - sphere Position
+	
+	//Sphere ray intersection
+	// |O + tD|^2 - R^2 = 0
+	// O^2 + (tD)^2 + 2ODt - R^2 = 0
+	// O^2 + D^2t^2 + 2ODt - R^2 = 0
+	// D^2T^2 + 2ODt - (O^2 - R^2) = 0 <-- quadriatic equation
+	// quadriatic equation = ax^2 + bx + c
+	// a = D^2
+	// b = 2OD
+	// c = O^2 - R ^2
+	// delta = b^2 - 4ac
+	// if(delta > 0.0)
+	//  tClosest = (-b - sqrt) / 2a
+	//  tFurther = (-b + sqrt) / 2a
+	// else if(delta == 0.0)
+	//	tClosest = -b/2a
+	// else no hit 
+
+	//Once Sphere Center is added 
+	// |O + tD^2 - C|^2 - R^2 = 0
+	// a = D^2 = 1 (since we assume normalized RayDirection)
+	// b = 2D(O-C)
+	// c = |O-C|^2 - R^2
+
+	//This assumes Ray Dir is normalized -> A = 1.0
+	glm::vec3 L = ray.Origin - Position;
+	float b = 2.0f * glm::dot(ray.Direction, L);
+	float c = glm::dot(L, L) - (Radius * Radius);
+	float discriminant = (b * b) - 4 * c;//(delta)
+	if (discriminant < 0.0f)
+	{
+		return Trace().Miss();
+	}
+	else if (discriminant > 0.0f)
+	{
+		float closerT = (-b - glm::sqrt(discriminant)) / 2.0f;
+		//float furtherT = (-b + glm::sqrt(discriminant)) / 2.0f;
+		return Trace().Hit(closerT, Guid);
+	}
+	else
+	{
+		float T = -b / 2.0f;
+		return Trace().Hit(T, Guid);
+	}
+}
+
+Trace Sphere::TraceRay(const Ray& ray) const
+{
+	return AnalyticSolution(ray);
+	//return GeometricSolution(ray);
+}
+
 Trace Sphere::ResolveTracePayload(Trace& trace, const Ray& ray) const
 {
 	trace.WorldPosition = ray.Origin + ray.Direction * trace.HitDistance;
 	trace.WorldNormal = glm::normalize(trace.WorldPosition - Position);
 	return trace;
 }
+
+
 
--- Raytraycer/src/Scene/Geometry/Sphere.h
+++ Raytraycer/src/Scene/Geometry/Sphere.h
@@ -9,4 +9,7 @@ public:
 	virtual void BuildUI(uint32_t id) override;
 	virtual Trace TraceRay(const Ray& ray) const override;
 	virtual Trace ResolveTracePayload(Trace& payload, const Ray& ray) const override;
+private:
+	Trace GeometricSolution(const Ray& ray) const;
+	Trace AnalyticSolution(const Ray& ray) const;
 };
